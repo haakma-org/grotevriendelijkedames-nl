51783df959fbdb35e5a05bb14f102746fbd56787
sido <sido@haakma.org>
2017-09-13 22:33:50 +0200

    [FIX] deleted critical information

--- grotevriendelijkedames.nl-site/backup/backup_gvd-site.sh
+++ grotevriendelijkedames.nl-site/backup/backup_gvd-site.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-DATE=`date +%Y-%m-%d`
-BASE_DIR=/home/sido/backup/today
+SQL_PASSWORD=${1}
+SQL_SCHEMA=${2}
 
-mysqldump -pKoRn0512 gvd > ${BASE_DIR}/${DATE}_gvd.sql
+DATE=`date +%Y-%m-%d`
+BASE_DIR=/home/backup/today
+
+mysqldump -p${SQL_PASSWORD} ${SQL_SCHEMA} > ${BASE_DIR}/${DATE}_gvd.sql
 
 tar -cvf ${BASE_DIR}/${DATE}_gvd.tar /srv/www/grotevriendelijkedames.nl/
 
