699dda64eb4f9650125fb6ca0730f08767b584f5
sido <sido@haakma.org>
2017-09-13 23:13:03 +0200

    [FEATURE] read user properties from file

--- grotevriendelijkedames.nl-site/backup/backup_gvd-site.sh
+++ grotevriendelijkedames.nl-site/backup/backup_gvd-site.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-SQL_PASSWORD=${1}
-SQL_SCHEMA=${2}
+. ../../utils/read_configuration.sh
+
+read_config "/home/config/gvd.properties"
 
 DATE=`date +%Y-%m-%d`
 BASE_DIR=/home/backup/today
 WWW_DIR=/home/sites
 
-mysqldump -p${SQL_PASSWORD} ${SQL_SCHEMA} > ${BASE_DIR}/${DATE}_gvd.sql
+echo "[INFO] Backup MySQL schema : ${SQL_SCHEMA}"
+mysqldump -u ${SQL_USER} -p${SQL_PASSWORD} ${SQL_SCHEMA} > ${BASE_DIR}/${DATE}_gvd.sql
 
 tar -cvf ${BASE_DIR}/${DATE}_gvd.tar ${WWW_DIR}/grotevriendelijkedames.nl/
 
