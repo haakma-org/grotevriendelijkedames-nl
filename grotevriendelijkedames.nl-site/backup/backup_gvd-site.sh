a3b7e07edac70357fd01a9f1cff0e57726f5f13b
sido <sido@haakma.org>
2017-09-25 22:41:34 +0200

    [FIX] updated backup directory

--- grotevriendelijkedames.nl-site/backup/backup_gvd-site.sh
+++ grotevriendelijkedames.nl-site/backup/backup_gvd-site.sh
@@ -14,5 +14,5 @@ echo "[INFO] Backup MySQL schema : ${SQL_SCHEMA}"
 mysqldump -u ${SQL_USER} -p${SQL_PASSWORD} ${SQL_SCHEMA} --host=${SQL_HOST} --port=${SQL_PORT} > ${BASE_DIR}/${DATE}__gvd.sql
 
 echo "[INFO] Backup data directory : ${WWW_DIR}/wwww.grotevriendelijkdames.nl/"
-tar -cvf ${BASE_DIR}/${DATE}_gvd.tar ${WWW_DIR}/grotevriendelijkedames.nl/
+tar -cvf ${BASE_DIR}/${DATE}_gvd.tar ${WWW_DIR}/www.grotevriendelijkedames.nl/
 
