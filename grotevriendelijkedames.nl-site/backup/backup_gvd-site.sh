129d80c59a536cc743b89baf5c7f38d8c494e6dc
sido <sido@haakma.org>
2017-09-25 23:19:14 +0200

    [FIX] removed underscore in sql file

--- grotevriendelijkedames.nl-site/backup/backup_gvd-site.sh
+++ grotevriendelijkedames.nl-site/backup/backup_gvd-site.sh
@@ -11,7 +11,7 @@ BASE_DIR=/home/backup/grotevriendelijkedames.nl/today
 WWW_DIR=/home/sites
 
 echo "[INFO] Backup MySQL schema : ${SQL_SCHEMA}"
-mysqldump -u ${SQL_USER} -p${SQL_PASSWORD} ${SQL_SCHEMA} --host=${SQL_HOST} --port=${SQL_PORT} > ${BASE_DIR}/${DATE}__gvd.sql
+mysqldump -u ${SQL_USER} -p${SQL_PASSWORD} ${SQL_SCHEMA} --host=${SQL_HOST} --port=${SQL_PORT} > ${BASE_DIR}/${DATE}_gvd.sql
 
 echo "[INFO] Backup data directory : ${WWW_DIR}/wwww.grotevriendelijkdames.nl/"
 tar -cvf ${BASE_DIR}/${DATE}_gvd.tar ${WWW_DIR}/www.grotevriendelijkedames.nl/
