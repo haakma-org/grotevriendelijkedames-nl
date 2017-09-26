5cf38561b812d500fc5a245bbfc0344b894c550d
sido <sido@haakma.org>
2017-09-26 09:49:28 +0200

    [FIX] backup failes when database is not accessible

--- grotevriendelijkedames.nl-site/backup/backup_gvd-site.sh
+++ grotevriendelijkedames.nl-site/backup/backup_gvd-site.sh
@@ -12,6 +12,10 @@ WWW_DIR=/home/sites
 
 echo "[INFO] Backup MySQL schema : ${SQL_SCHEMA}"
 mysqldump -u ${SQL_USER} -p${SQL_PASSWORD} ${SQL_SCHEMA} --host=${SQL_HOST} --port=${SQL_PORT} > ${BASE_DIR}/${DATE}_gvd.sql
+if [[ ${?} != 0 ]]; then
+  echo "[ERROR] mysqldump failed"
+  exit 1
+fi
 
 echo "[INFO] Backup data directory : ${WWW_DIR}/wwww.grotevriendelijkdames.nl/"
 tar -cvf ${BASE_DIR}/${DATE}_gvd.tar ${WWW_DIR}/www.grotevriendelijkedames.nl/
